8d6632dd8884c49194d36fff7219b67a3104146c
sophie-apples <sophieannemurray@gmail.com>
2020-09-25 08:17:26 +0000

    chat page with refresh and messages displayed from dictionary and js on chat page

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import os
 from datetime import datetime
 from flask import Flask, redirect, render_template, request, session
 
+
 app = Flask(__name__)
 app.secret_key = "randomstring123"
 messages = []
@@ -10,18 +11,13 @@ messages = []
 def add_messages(username, message):
     """Add messages to the `messages` list"""
     now = datetime.now().strftime("%H:%M:%S")
-    messages.append("({}) {}: {}".format(now, username, message))
+    messages_dict = {"timestamp": now, "from": username, "message": message}
+    messages.append(messages_dict)
 
 
-def get_all_messages():
-    """Get all of the messages and separate them with a `br`"""
-    return "<br>".join(messages)
-
-
-@app.route("/", methods= ["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def index():
     """Main page with instructions"""
-
     if request.method == "POST":
         session["username"] = request.form["username"]
 
@@ -34,7 +30,8 @@ def index():
 @app.route("/<username>")
 def user(username):
     """Display chat messages"""
-    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
+    return render_template("chat.html", username=username,
+                           chat_messages=messages)
 
 
 @app.route("/<username>/<message>")
